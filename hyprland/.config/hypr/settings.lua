0929092da2b20855d2a49b5b4b311ae308095c6f
Brian Sokol <bri@nsokol.net>
2026-06-15 16:30:59 -0500

    feat(hypr): dim inactive windows

--- hyprland/.config/hypr/settings.lua
+++ hyprland/.config/hypr/settings.lua
@@ -42,7 +42,7 @@ hl.config({
     rounding = 10,
     rounding_power = 2,
     active_opacity = 1.0,
-    inactive_opacity = 1.0,
+    inactive_opacity = 0.94,
     shadow = {
       enabled = true,
       range = 4,
